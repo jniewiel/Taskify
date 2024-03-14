7c8ce1ec1de14749283a4bb4789051f538097cf7
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-03-14 03:12:48 +0000

    Added unit testing file 'test_main.rb' to test tasks.rb

---
+++ tests/test_main.rb
@@ -0,0 +1,26 @@
+#tests/test_tasks.rb
+
+require 'minitest/autorun'
+require './tasks'
+
+class TestTasks < Minitest::Test
+  
+  def setup
+    @main = Main.new
+  end
+
+  def test_delete_category
+    @main.delete_category("Completed")
+    assert_equal 1, @main.categories.size, "Category should be deleted"
+  end
+
+  def test_add_category
+    @main.add_category("test")
+    assert_equal 3, @main.categories.size, "Category should be added"
+  end
+
+  def test_view_all
+    assert_output(/Main:\n  - Take out trash\n  - Make reservations\n  - Practice Ruby coding\n\nCompleted:/) {@main.view_all}
+  end
+
+end
